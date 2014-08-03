41315b953336e44966585788c90bab6e9f98df58
Jordan Grieve <quintilex@gmail.com>
2014-08-03 14:43:07 -0400

    starting database values

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'exchores',
+			'username'  => 'root',
+			'password'  => 'G3IujRwj',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
