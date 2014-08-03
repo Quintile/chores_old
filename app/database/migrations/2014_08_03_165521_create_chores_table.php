f7b56a7c0653e55d2f999106217afe1b7ad1af26
Jordan Grieve <quintilex@gmail.com>
2014-08-03 14:40:44 -0400

    setup database structure

---
+++ app/database/migrations/2014_08_03_165521_create_chores_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChoresTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('chores', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('room_id');
+			$table->string("name");
+			$table->text("description")->nullable();
+			$table->integer("duration");
+			$table->timestamp('lastdone')->nullable();
+			$table->integer('frequency');
+			$table->integer('importance');
+			$table->integer('user_id')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('chores');
+	}
+
+}
