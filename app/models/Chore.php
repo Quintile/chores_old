cfc4f6dced90e03fcd7a9eabfe34237e30593354
Jordan Grieve <quintilex@gmail.com>
2014-08-03 22:08:34 -0400

    added helper methods to Chore model

--- app/models/Chore.php
+++ app/models/Chore.php
@@ -5,4 +5,43 @@ namespace Models;
 class Chore extends \Eloquent
 {
 	protected $guarded = array();
+
+	public function room()
+	{
+		return $this->belongsTo('\Models\Room');
+	}
+
+	public function days()
+	{
+		if(is_null($this->lastdone))
+			return null;
+
+		$now = new \DateTime();
+		$diff = $now->diff(new \DateTime($this->lastdone))->format("%a");
+		return $diff;
+	}
+
+	public function priority()
+	{
+		if(is_null($this->days()))
+			$days = $this->frequency;
+		else
+			$days = $this->days();
+
+		$priority = $days / $this->frequency;
+		
+		return number_format($priority, 2);
+	}
+
+	public function urgency()
+	{
+		$priority = $this->priority();
+		if($priority >= 1 )
+			return "danger";
+
+		if($priority >= 0.8 && $priority < 1)
+			return "warning";
+
+		return null;
+	}
 }
